1a1fb2f4315de4a2be099f8d9c41afb966840e1a
GustavoHerreroNunes <ghnunes2010@gmail.com>
2024-12-29 18:18:46 -0300

    Add: route constants and base login page

---
+++ lib/constants/routes_const.dart
@@ -0,0 +1,6 @@
+abstract class RoutesConst {
+  static const String home = '/home';
+  static const String logIn = '/';
+  static const String signUp = '/signup';
+  static const String forgotPass = '/forgot_password';
+}
